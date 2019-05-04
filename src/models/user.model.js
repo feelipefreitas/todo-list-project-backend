6e16c16b933c3afe7cac591f0deb8929347cfa6d
feelipefreitas <feelipe.freitas97@gmail.com>
2019-05-04 17:50:23 -0300

    Creating middleware to handle duplicate user email.

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -34,6 +34,13 @@ const userSchema = new mongoose.Schema({
    }
 });
 
+userSchema.post('save', function(error, doc, next) {
+    if (error.name === 'MongoError' && error.code === 11000) 
+        next(new Error('The email is already in use'));
+    
+    next();
+});
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -3,15 +3,19 @@ const router = new express.Router();
 
 const User = require('../models/user.model');
 
-router.post('/users', async (req, res) => {
+router.post('/users/register', async (req, res) => {
     const newUser = new User(req.body);
-    
+
     try {
         await newUser.save();
-        
-        res.status(201).send({ user: newUser });
+
+        res.status(201).send({
+            user: newUser
+        });
     } catch (e) {
-        res.status(400).send({ error: e });
+        res.status(400).send({
+            error: e.message
+        });
     }
 });
 
